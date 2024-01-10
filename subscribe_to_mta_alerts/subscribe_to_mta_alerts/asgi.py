7eec1438f7ba8cb80a0d89d78c0d209232908fb2
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-10 13:23:16 -0500

    Initial setup - create django project, venv, and gitignore

---
+++ subscribe_to_mta_alerts/subscribe_to_mta_alerts/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for subscribe_to_mta_alerts project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'subscribe_to_mta_alerts.settings')
+
+application = get_asgi_application()
