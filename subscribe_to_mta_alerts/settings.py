499c840b0d8b7fda3ea93595577fb533e41a2221
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-10 14:03:41 -0500

    Intial setup for DB

--- subscribe_to_mta_alerts/settings.py
+++ subscribe_to_mta_alerts/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'subscribe_to_mta_alerts.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
