396e78093eb5402eb7971a9b90ad42e2bdb17194
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-19 21:32:57 -0500

    Update urls

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("mta_api/", include("mta_api.urls")),
+    path('api-auth/', include('rest_framework.urls')),
     path("admin/", admin.site.urls),
+    path("mta-api/", include("mta_api.urls")),
 ]
