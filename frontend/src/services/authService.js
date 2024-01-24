07d1da619c24bff15e8c161b0f7566ae1865f36f
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-24 16:00:32 -0500

    Add env variable for apiUrl

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,18 +1,14 @@
 import ky from "ky";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const login = async (username, password) => {
-  try {
-    const response = await ky
-      .post("http://127.0.0.1:8000/api-token-auth/", {
-        json: { username, password },
-      })
-      .json();
-    const token = response.token 
-    console.log(token)
-  } catch (error) {
-    console.error("Login failed:", error);
-    throw error;
-  }
+  const response = await ky
+    .post(`${apiUrl}/api-token-auth/`, {
+      json: { username, password },
+    })
+    .json();
+  const token = response.token;
+  console.log(token);
 };
 
 export default {
