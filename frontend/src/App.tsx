a2a24fcfc61858d9e82f3a88822718d7ad9af955
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-24 18:04:54 -0500

    Add home page for testing purposes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 import LoginForm from "./components/LoginForm/LoginForm";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/home" element={<Home/>} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
     </div>
