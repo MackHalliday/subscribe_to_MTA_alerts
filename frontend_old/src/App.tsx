9cb9701530b49499d1e14810962343285e716eae
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-25 16:36:39 -0500

    Old frontend using create-react-app - removed components and services, removed files for tailwind congif

---
+++ frontend_old/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../frontend/src/components/Home/Home";
+import LoginForm from "../../frontend/src/components/LoginForm/LoginForm";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App; 
