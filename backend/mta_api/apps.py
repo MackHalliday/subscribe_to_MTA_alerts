1085d3432c9cd1498ee734dd435d0cb4dacf2935
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-25 12:18:27 -0500

    Changes from black

--- backend/mta_api/apps.py
+++ backend/mta_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MtaApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mta_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mta_api"
