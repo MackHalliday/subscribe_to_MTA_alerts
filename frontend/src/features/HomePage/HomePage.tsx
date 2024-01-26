03bae15afbb794c9361d2126b038e786c1ca5808
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-25 22:32:08 -0500

    Add login form to home page and add page title

--- frontend/src/features/HomePage/HomePage.tsx
+++ frontend/src/features/HomePage/HomePage.tsx
@@ -1,5 +1,12 @@
+import LoginForm from '../../components/LoginForm/LoginForm'
+
 function HomePage() {
-  return <div> Home Page </div>
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-4">Home</h1>
+      <LoginForm />
+    </div>
+  )
 }
 
 export default HomePage
