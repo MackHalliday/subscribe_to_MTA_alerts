261d59b8edd027f8dbd5db74063a73e3831a6734
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-26 12:58:39 -0500

    Update syntax so uniform with rest of code

--- frontend/src/features/HomePage/HomePage.tsx
+++ frontend/src/features/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import LoginForm from '../../components/LoginForm/LoginForm'
+import React from 'react'
 
-function HomePage() {
+const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-4">Home</h1>
