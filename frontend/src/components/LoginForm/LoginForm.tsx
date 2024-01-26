191b50a12210865961120d3aa7f3e35e505f1138
Mack Halliday <Mackenzie.Halliday@gmail.com>
2024-01-25 22:35:39 -0500

    Stma 9 small adjustments design using tailwind (#20)

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -39,7 +39,7 @@ function LoginForm() {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-center">
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
