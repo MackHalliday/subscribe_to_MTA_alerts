b99ca845bdaf1673e23b112eeb9287d255ef8bd7
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2024-01-25 22:19:30 -0500

    Add margin to links in nav bar

--- frontend/src/components/BaseComponents/NavBar.tsx
+++ frontend/src/components/BaseComponents/NavBar.tsx
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom'
 const NavBar: React.FC = () => {
   return (
     <nav className="nav-bar">
-      <NavLink to="/" className="nav-link">
+      <NavLink to="/" className="nav-link m-2">
         Home
       </NavLink>
-      <NavLink to="/about" className="nav-link">
+      <NavLink to="/about" className="nav-link m-2">
         About
       </NavLink>
     </nav>
